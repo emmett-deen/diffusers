2b202e7988f72c0d971a9ee739fdaae8ed2169c0
Emmett Deen <emmett.deen@kickasscoding.com>
2023-06-16 19:28:34 -0400

    disable stable diffusion safety checker

--- src/diffusers/pipelines/stable_diffusion/safety_checker.py
+++ src/diffusers/pipelines/stable_diffusion/safety_checker.py
@@ -48,78 +48,80 @@ class StableDiffusionSafetyChecker(PreTrainedModel):
 
     @torch.no_grad()
     def forward(self, clip_input, images):
-        pooled_output = self.vision_model(clip_input)[1]  # pooled_output
-        image_embeds = self.visual_projection(pooled_output)
+        return images, False
+        # pooled_output = self.vision_model(clip_input)[1]  # pooled_output
+        # image_embeds = self.visual_projection(pooled_output)
 
-        # we always cast to float32 as this does not cause significant overhead and is compatible with bfloat16
-        special_cos_dist = cosine_distance(image_embeds, self.special_care_embeds).cpu().float().numpy()
-        cos_dist = cosine_distance(image_embeds, self.concept_embeds).cpu().float().numpy()
+        # # we always cast to float32 as this does not cause significant overhead and is compatible with bfloat16
+        # special_cos_dist = cosine_distance(image_embeds, self.special_care_embeds).cpu().float().numpy()
+        # cos_dist = cosine_distance(image_embeds, self.concept_embeds).cpu().float().numpy()
 
-        result = []
-        batch_size = image_embeds.shape[0]
-        for i in range(batch_size):
-            result_img = {"special_scores": {}, "special_care": [], "concept_scores": {}, "bad_concepts": []}
+        # result = []
+        # batch_size = image_embeds.shape[0]
+        # for i in range(batch_size):
+        #     result_img = {"special_scores": {}, "special_care": [], "concept_scores": {}, "bad_concepts": []}
 
-            # increase this value to create a stronger `nfsw` filter
-            # at the cost of increasing the possibility of filtering benign images
-            adjustment = 0.0
+        #     # increase this value to create a stronger `nfsw` filter
+        #     # at the cost of increasing the possibility of filtering benign images
+        #     adjustment = 0.0
 
-            for concept_idx in range(len(special_cos_dist[0])):
-                concept_cos = special_cos_dist[i][concept_idx]
-                concept_threshold = self.special_care_embeds_weights[concept_idx].item()
-                result_img["special_scores"][concept_idx] = round(concept_cos - concept_threshold + adjustment, 3)
-                if result_img["special_scores"][concept_idx] > 0:
-                    result_img["special_care"].append({concept_idx, result_img["special_scores"][concept_idx]})
-                    adjustment = 0.01
+        #     for concept_idx in range(len(special_cos_dist[0])):
+        #         concept_cos = special_cos_dist[i][concept_idx]
+        #         concept_threshold = self.special_care_embeds_weights[concept_idx].item()
+        #         result_img["special_scores"][concept_idx] = round(concept_cos - concept_threshold + adjustment, 3)
+        #         if result_img["special_scores"][concept_idx] > 0:
+        #             result_img["special_care"].append({concept_idx, result_img["special_scores"][concept_idx]})
+        #             adjustment = 0.01
 
-            for concept_idx in range(len(cos_dist[0])):
-                concept_cos = cos_dist[i][concept_idx]
-                concept_threshold = self.concept_embeds_weights[concept_idx].item()
-                result_img["concept_scores"][concept_idx] = round(concept_cos - concept_threshold + adjustment, 3)
-                if result_img["concept_scores"][concept_idx] > 0:
-                    result_img["bad_concepts"].append(concept_idx)
+        #     for concept_idx in range(len(cos_dist[0])):
+        #         concept_cos = cos_dist[i][concept_idx]
+        #         concept_threshold = self.concept_embeds_weights[concept_idx].item()
+        #         result_img["concept_scores"][concept_idx] = round(concept_cos - concept_threshold + adjustment, 3)
+        #         if result_img["concept_scores"][concept_idx] > 0:
+        #             result_img["bad_concepts"].append(concept_idx)
 
-            result.append(result_img)
+        #     result.append(result_img)
 
-        has_nsfw_concepts = [len(res["bad_concepts"]) > 0 for res in result]
+        # has_nsfw_concepts = [len(res["bad_concepts"]) > 0 for res in result]
 
-        for idx, has_nsfw_concept in enumerate(has_nsfw_concepts):
-            if has_nsfw_concept:
-                if torch.is_tensor(images) or torch.is_tensor(images[0]):
-                    images[idx] = torch.zeros_like(images[idx])  # black image
-                else:
-                    images[idx] = np.zeros(images[idx].shape)  # black image
+        # for idx, has_nsfw_concept in enumerate(has_nsfw_concepts):
+        #     if has_nsfw_concept:
+        #         if torch.is_tensor(images) or torch.is_tensor(images[0]):
+        #             images[idx] = torch.zeros_like(images[idx])  # black image
+        #         else:
+        #             images[idx] = np.zeros(images[idx].shape)  # black image
 
-        if any(has_nsfw_concepts):
-            logger.warning(
-                "Potential NSFW content was detected in one or more images. A black image will be returned instead."
-                " Try again with a different prompt and/or seed."
-            )
+        # if any(has_nsfw_concepts):
+        #     logger.warning(
+        #         "Potential NSFW content was detected in one or more images. A black image will be returned instead."
+        #         " Try again with a different prompt and/or seed."
+        #     )
 
-        return images, has_nsfw_concepts
+        # return images, has_nsfw_concepts
 
     @torch.no_grad()
     def forward_onnx(self, clip_input: torch.FloatTensor, images: torch.FloatTensor):
-        pooled_output = self.vision_model(clip_input)[1]  # pooled_output
-        image_embeds = self.visual_projection(pooled_output)
+        return images, False
+        # pooled_output = self.vision_model(clip_input)[1]  # pooled_output
+        # image_embeds = self.visual_projection(pooled_output)
 
-        special_cos_dist = cosine_distance(image_embeds, self.special_care_embeds)
-        cos_dist = cosine_distance(image_embeds, self.concept_embeds)
+        # special_cos_dist = cosine_distance(image_embeds, self.special_care_embeds)
+        # cos_dist = cosine_distance(image_embeds, self.concept_embeds)
 
-        # increase this value to create a stronger `nsfw` filter
-        # at the cost of increasing the possibility of filtering benign images
-        adjustment = 0.0
+        # # increase this value to create a stronger `nsfw` filter
+        # # at the cost of increasing the possibility of filtering benign images
+        # adjustment = 0.0
 
-        special_scores = special_cos_dist - self.special_care_embeds_weights + adjustment
-        # special_scores = special_scores.round(decimals=3)
-        special_care = torch.any(special_scores > 0, dim=1)
-        special_adjustment = special_care * 0.01
-        special_adjustment = special_adjustment.unsqueeze(1).expand(-1, cos_dist.shape[1])
+        # special_scores = special_cos_dist - self.special_care_embeds_weights + adjustment
+        # # special_scores = special_scores.round(decimals=3)
+        # special_care = torch.any(special_scores > 0, dim=1)
+        # special_adjustment = special_care * 0.01
+        # special_adjustment = special_adjustment.unsqueeze(1).expand(-1, cos_dist.shape[1])
 
-        concept_scores = (cos_dist - self.concept_embeds_weights) + special_adjustment
-        # concept_scores = concept_scores.round(decimals=3)
-        has_nsfw_concepts = torch.any(concept_scores > 0, dim=1)
+        # concept_scores = (cos_dist - self.concept_embeds_weights) + special_adjustment
+        # # concept_scores = concept_scores.round(decimals=3)
+        # has_nsfw_concepts = torch.any(concept_scores > 0, dim=1)
 
-        images[has_nsfw_concepts] = 0.0  # black image
+        # images[has_nsfw_concepts] = 0.0  # black image
 
-        return images, has_nsfw_concepts
+        # return images, has_nsfw_concepts
